50c3b59b40a829c8a2b77283a278e68a7fa15dfd
Felix Nyandieka <nyandiekafelix@gmail.com>
2020-08-05 11:16:22 +0300

    fix dependency production error

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
-import VueSocketIO from 'vue-socket.io';
+import VueSocketIO from 'vue-socket.io-extended';
+import socketioClient from 'socket.io-client';
+
+const url = process.env.NODE_ENV === 'production' ?
+  'https://tele.motocle2.com:8989' :
+  'http://localhost:8989';
+
+const connection = socketioClient(url, { secure: true });
 
 export default function () {
- Vue.use(new VueSocketIO({
-   debug: false,
-   connection: 'https://tele.motocle2.com:8989',
- }));
+ Vue.use(VueSocketIO, connection);
 }
